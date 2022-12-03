9a56fcd5cbca18105040510da4da216070a029c8
Christoph Berg <myon@debian.org>
2022-12-03 16:46:39 +0100

    Add 8m and 5m bands

--- sql/band.sql
+++ sql/band.sql
@@ -1,7 +1,7 @@
 create type rrdxa.band AS enum (
     '2190m', '630m', '560m',
     '160m', '80m', '60m', '40m', '30m', '20m', '17m', '15m', '12m', '10m',
-    '6m', '4m', '2m', '1.25m',
+    '8m', '6m', '4m', '5m', '2m', '1.25m',
     '70cm', '33cm', '23cm', '13cm', '9cm', '6cm', '3cm', '1.25cm',
     '6mm', '4mm', '2.5mm', '2mm', '1mm',
     'unknown'
@@ -10,20 +10,25 @@ create type rrdxa.band AS enum (
 create or replace function rrdxa.band(freq numeric) returns band
 language sql
 return case
-    when freq between 0.136 and 0.137       then '2190m'::band
+    when freq between 0.1357 and 0.1378     then '2190m'::rrdxa.band
     when freq between 0.472 and 0.479       then '630m'
     when freq between 0.501 and 0.504       then '560m'
-    when freq between 1.8 and 2.0           then '160m' -- lower end rounded down
+    when freq between 1.8 and 2.0           then '160m'
     when freq between 3.5 and 4.0           then '80m'
-    when freq between 5.102 and 5.4065      then '60m'
+    when freq between 5.06 and 5.45         then '60m'
     when freq between 7.0 and 7.3           then '40m'
-    when freq between 10.0 and 10.15        then '30m' -- lower end rounded down
+    when freq = 10.0                        then '30m'
+    when freq between 10.1 and 10.15        then '30m'
     when freq between 14.0 and 14.35        then '20m'
-    when freq between 18.0 and 18.168       then '17m' -- lower end rounded down
+    when freq = 18.0                        then '17m'
+    when freq between 18.068 and 18.168     then '17m'
     when freq between 21.0 and 21.45        then '15m'
+    when freq = 24.0                        then '12m'
     when freq between 24.89 and 24.99       then '12m'
     when freq between 28.0 and 29.7         then '10m'
+    when freq between 40.0 and 45.0         then '8m'
     when freq between 50.0 and 54.0         then '6m'
+    when freq between 54.000001 and 69.9    then '5m'
     when freq between 70.0 and 71.0         then '4m'
     when freq between 144.0 and 148.0       then '2m'
     when freq between 222.0 and 225.0       then '1.25m'
@@ -43,4 +48,4 @@ return case
     else 'unknown'
 end;
 
-create cast (numeric as band) with function band as assignment;
+create cast (numeric as rrdxa.band) with function rrdxa.band as assignment;
