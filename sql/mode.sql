4a8101474629718eb774722446d4e1e2b9722a4d
Christoph Berg <myon@debian.org>
2022-12-03 16:46:53 +0100

    Add A1A and J3E

--- sql/mode.sql
+++ sql/mode.sql
@@ -4,8 +4,10 @@ create or replace function rrdxa.major_mode(submode text)
     returns mode
     language sql
     return case
-        when upper(submode) = 'CW' then 'CW'::mode
-        when upper(submode) in ('PHONE', 'SSB', 'LSB', 'USB', 'FM', 'AM',
+        when upper(submode) in ('CW', 'CW-R', 'A1A') then 'CW'::mode
+        when upper(submode) in ('PHONE', 'PH',
+            'SSB', 'LSB', 'USB', 'J3E',
+            'FM', 'F3E', 'AM',
             'DIGITALVOICE', 'C4FM', 'DMR', 'DSTAR') then 'PHONE'
         else 'DIGI'
     end;
