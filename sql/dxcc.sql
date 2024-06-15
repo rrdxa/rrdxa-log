1c90c7307a3ca15ba05c531832f31100602b102a
Christoph Berg <myon@debian.org>
2024-06-15 17:30:52 +0200

    Add continent type

--- sql/dxcc.sql
+++ sql/dxcc.sql
@@ -1,7 +1,10 @@
+create type rrdxa.continent as enum ('NA', 'SA', 'EU', 'AF', 'OC', 'AS', 'AN');
+
 create table rrdxa.dxcc (
     dxcc smallint primary key,
     country text not null,
-    deleted boolean not null
+    deleted boolean not null,
+    cont rrdxa.continent
 );
 
 \copy rrdxa.dxcc (dxcc, country, deleted) from 'sql/dxcc.data'
