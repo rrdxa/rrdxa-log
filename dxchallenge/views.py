5c15c5bc38d5d1ea224ed5c05bfa455d1393346e
Christoph Berg <myon@debian.org>
2024-11-22 22:23:05 +0100

    Show worked countries on map

--- dxchallenge/views.py
+++ dxchallenge/views.py
@@ -5,6 +5,7 @@ from django.db import connection
 import psycopg2, psycopg2.extras
 psycopg2.extensions.register_adapter(dict, psycopg2.extras.Json)
 
+import json
 import datetime
 import re
 
@@ -14,8 +15,8 @@ from rrlog.utils import namedtuplefetchall, band_sort
 
 
 q_dxchallenge = """
-select *, rank() over (partition by major_mode order by count desc) from
-(select major_mode, rrmember, count(*) from bandpoints where year = %s group by 1, 2);
+select *, rank() over (partition by major_mode order by bandslots desc) from
+(select major_mode, rrmember, count(*) as bandslots, array_agg(distinct dxcc) as dxccs from bandpoints where year = %s group by 1, 2);
 """
 
 def v_dxchallenge(request, year=None):
@@ -23,17 +24,20 @@ def v_dxchallenge(request, year=None):
         today = datetime.date.today()
         year = today.year
 
-    entries = {"CW": [], "PHONE": [], "DIGI": [], "FT8": [], "unknown": []}
+    entries = {"CW": [], "PHONE": [], "DIGI": [], "FT8": []}
+    data = {"CW": {}, "PHONE": {}, "DIGI": {}, "FT8": {}}
     with connection.cursor() as cursor:
         cursor.execute(q_dxchallenge, [year])
-        for row in cursor.fetchall():
-            entries[row[0]].append({'call': row[1], 'bandslots': row[2], 'rank': row[3]})
+        for row in namedtuplefetchall(cursor):
+            entries[row.major_mode].append({'call': row.rrmember, 'bandslots': row.bandslots, 'rank': row.rank})
+            data[row.major_mode][row.rrmember] = row.dxccs
 
     context = {
         'title': f"RRDXA DX Challenge {year}",
         'year': year,
         'modes': ['CW', 'PHONE', 'DIGI', 'FT8'],
         'entries': entries,
+        'data': json.dumps(data),
     }
     return render(request, 'dxchallenge/dxchallenge.html', context)
 
