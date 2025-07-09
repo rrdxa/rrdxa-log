rrdxa/rrdxa-log

commit a42f67cab555cda600ea5ebad201449935b01685

Make adif uploads not overwrite cabrillo upload numbers

rrlog/adif_upload.py
@@ -17,7 +17,6 @@ rstrx =      coalesce(excluded.rstrx, log.rstrx),
 exrx =       coalesce(excluded.exrx, log.exrx),
 gridsquare = coalesce(excluded.gridsquare, log.gridsquare),
 contest =    coalesce(excluded.contest, log.contest),
-upload = excluded.upload,
 adif = excluded.adif
 """
 

rrlog/templates/rrlog/upload.html
@@ -145,7 +145,8 @@ Feel free to upload overlapping logfiles or to re-upload logfiles when some of
 the contents have changed. The new logfile will be merged into the database,
 replacing any already existing QSOs. QSOs are identified by matching time
 stamp, station callsign, DX callsign, band and major mode (CW, PHONE, DIGI, FT8).
-QSOs are associated with the last upload that contained them; deleting an
+QSOs are associated with the first ADIF upload (Cabrillo: last upload) that
+contained them; deleting an
 upload will only delete the QSOs still associated with it.
 </li>
 </ul>

commit c65e4129e7f0a2d3c84b4516f11488ec9251aa4a

Make adif contest log uploads overwrite log.upload
Fixes: a42f67cab555cda600ea5ebad201449935b01685

rrlog/adif_upload.py
@@ -18,10 +18,11 @@ rstrx =      coalesce(excluded.rstrx, log.rstrx),
 exrx =       coalesce(excluded.exrx, log.exrx),
 gridsquare = coalesce(excluded.gridsquare, log.gridsquare),
 contest =    coalesce(excluded.contest, log.contest),
+upload =     case when %s then excluded.upload else log.upload end,
 adif = excluded.adif
 """
 
-def adif_upload(cursor, content, station_callsign, operator, upload_id):
+def adif_upload(cursor, content, station_callsign, operator, upload_id, contest):
     with transaction.atomic():
         qsos, adif_headers = adif_io.read_from_string(content)
         upload_start, upload_stop = None, None
@@ -70,6 +71,7 @@ def adif_upload(cursor, content, station_callsign, operator, upload_id):
                             qso.get('CONTEST_ID'),
                             upload_id,
                             json.dumps(qso),
+                            contest # contest uploads overwrite old upload ids, other uploads do not
                             ])
         cursor.execute("update upload set qsos = %s, start = date_trunc('minute', %s), stop = date_trunc('minute', %s) where id = %s", [len(qsos), upload_start, upload_stop, upload_id])
 

rrlog/upload.py
@@ -10,7 +10,7 @@ def log_upload(connection, request, username):
 
     if '<EOR>' in upper_content:
         logtype = 'adif'
-        contest = data.get('contest') or False
+        contest = True if data.get('contest') else False
     elif 'START-OF-LOG' in upper_content:
         logtype = 'cabrillo'
         contest = True
@@ -27,7 +27,7 @@ def log_upload(connection, request, username):
 
         try:
             if logtype == 'adif':
-                num_qsos = adif_upload(cursor, content, station_callsign, operator, upload_id)
+                num_qsos = adif_upload(cursor, content, station_callsign, operator, upload_id, contest)
             elif logtype == 'cabrillo':
                 num_qsos = cabrillo_upload(cursor, content, upload_id)
 