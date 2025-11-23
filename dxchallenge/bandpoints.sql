4eaf1834e0bf4a46caf12bb2163c9292d5d552a0
Christoph Berg <myon@debian.org>
2025-11-23 22:09:51 +0100

    Remove bandpoints_history
    The query used in the dxchallenge mail did not compute the correct
    number for MIXED, and I never got to implement the history in the web
    interface.

--- dxchallenge/bandpoints.sql
+++ dxchallenge/bandpoints.sql
@@ -1,5 +1,17 @@
 create materialized view rrdxa.bandpoints as
-select * from (
+select
+year,
+    rrmember,
+    start,
+    station_callsign,
+    operator,
+    major_mode,
+    mode,
+    submode,
+    band,
+    call,
+    dxcc
+from (
     select extract(year from start) as year, rrcalls.rroperator as rrmember, start::date, station_callsign, operator, major_mode, mode, submode, band, call, dxcc,
         row_number() over (partition by extract(year from start), rrcalls.rroperator, major_mode, band, dxcc order by start)
     from log
@@ -8,29 +20,7 @@ select * from (
         major_mode <> 'unknown' and
         band <> 'unknown' and
         dxcc between 1 and 950
-) where row_number = 1;
+) where row_number = 1
+order by year, major_mode, rrmember;
 
-create table rrdxa.previous_bandpoints as
-select * from rrdxa.bandpoints;
-
-create index on rrdxa.previous_bandpoints (major_mode, dxcc, band);
-
-create table rrdxa.bandpoints_history (
-    date date,
-    rrmember text,
-    major_mode mode_t,
-    band band,
-    count int,
-    dxccs smallint[],
-    rank int,
-    constraint bandpoints_history_key unique nulls distinct (date, rrmember, major_mode, band)
-);
-
-    select distinct start::date, major_mode, band, dxcc, count(*)
-    from log
-    where
-        major_mode <> 'unknown' and
-        band <> 'unknown' and
-        dxcc between 1 and 950
-    group by 1, 2, 3, 4
-    order by 1, 2, 3, 4;
+create index on rrdxa.bandpoints(year);
