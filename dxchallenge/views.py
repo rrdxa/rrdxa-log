859403d1d59ceaed607568dc116ebcbce69db59a
Christoph Berg <myon@debian.org>
2025-11-23 23:25:45 +0100

    Add band selectors to dxchallenge

--- dxchallenge/views.py
+++ dxchallenge/views.py
@@ -17,6 +17,7 @@ select coalesce(major_mode::text, 'MIXED') as major_mode, rrmember, bandslots, d
 (select major_mode, rrmember, count(distinct (band, dxcc)) as bandslots, array_agg(distinct dxcc) as dxccs
     from bandpoints
     where year = %s
+        and (band = %s or %s is null)
     group by grouping sets ((1, 2), (2)));
 """
 
@@ -25,17 +26,24 @@ def v_dxchallenge(request, year=None):
         today = datetime.date.today()
         year = today.year
 
+    band = request.GET['band'] if 'band' in request.GET else None
+
     entries = {x: [] for x in major_modes}
     data = {x: {} for x in major_modes}
     with connection.cursor() as cursor:
-        cursor.execute(q_dxchallenge, [year])
+        cursor.execute(q_dxchallenge, [year, band, band])
         for row in namedtuplefetchall(cursor):
             entries[row.major_mode].append({'call': row.rrmember, 'bandslots': row.bandslots, 'rank': row.rank})
             data[row.major_mode][row.rrmember] = row.dxccs
 
+        cursor.execute("select distinct band from bandpoints where year = %s order by 1", [year])
+        bands = [x[0] for x in cursor.fetchall()]
+
     context = {
-        'title': f"RRDXA DX Challenge {year}",
+        'title': f"RRDXA DX Challenge {year} {band or ''}",
         'year': year,
+        'band': band,
+        'bands': bands,
         'modes': major_modes,
         'entries': entries,
         'data': json.dumps(data),
