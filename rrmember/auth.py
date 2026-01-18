272a234ef6cd21fa2b0c7b4d7baf0b165356b226
Christoph Berg <myon@debian.org>
2026-01-18 22:59:32 +0100

    Clean up debugging messages

--- rrmember/auth.py
+++ rrmember/auth.py
@@ -8,41 +8,43 @@ from .models import Member
 
 class WordpressAuthBackend(BaseBackend):
     def get_user(self, user_id):
-        print("hey get_user")
         try:
             user = User.objects.get(pk=user_id)
-            print("get_user", user)
             return user
         except User.DoesNotExist:
-            print("get_user DoesNotExist")
             return None
 
     def authenticate(self, request, username=None, password=None):
-        print("hey authenticate")
+        username = username.upper()
+
         try:
-            user = Member.objects.get(call=username)
+            wpuser = Member.objects.get(call=username)
         except Member.DoesNotExist:
-            print("no member found")
+            print(f"Login failed for {username}, user does not exist")
             return None
 
         # Wordpress 6.8 switched from phpass ($P$) to custom sha256+bcrypt ($wp$2y$).
         # Since that isn't supported by passlib yet, we deploy
         # https://wordpress.org/plugins/password-hash/ on the Wordpress side so
         # it's writing standard bcrypt ($2y$) now.
-        if user.user_pass[:2] == '$2':
+        if wpuser.user_pass[:2] == '$2':
             hash_method = bcrypt
-        elif user.user_pass[:3] == '$P$':
+        elif wpuser.user_pass[:3] == '$P$':
             hash_method = phpass
         else:
-            raise Exception(f"unknown password hash scheme for user {user}")
+            raise Exception(f"unknown password hash scheme for user {wpuser}")
 
-        if not hash_method.verify(password, user.user_pass):
+        if not hash_method.verify(password, wpuser.user_pass):
+            print(f"Login failed for {username}, wrong password")
             return None
 
         DjangoUser = get_user_model()
-        django_user, created = DjangoUser.objects.get_or_create(username=user.call)
-        if created:
-            django_user.is_staff = user.admin
-            django_user.save()
-        print('user:', django_user)
+        django_user, _ = DjangoUser.objects.get_or_create(
+                username=wpuser.call,
+                defaults={
+                    'first_name': wpuser.first_name,
+                    'last_name': wpuser.last_name,
+                    'email': wpuser.user_email,
+                    'is_staff': wpuser.admin,
+                })
         return django_user
